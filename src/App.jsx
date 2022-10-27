1f709a5139a09d9d8ed7e7261beb0cd9599abf20
Nemanja Klinic <klinicnemanja@gmail.com>
2022-10-27 10:59:04 +0200

    feat: responsive design with media query

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import Login from "./components/Login/Login.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
+
 function App() {
   return (
     <BrowserRouter>
+      <header className="logo"></header>
       <Routes>
         <Route path="/" element={<Login></Login>}/>
         <Route path="movies" element={<p>Movies</p>}/>
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <li>
+        
+      </li>
+    </>
+  );
+}
+
+export default Navbar;
